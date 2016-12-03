b0ecece751f320e1f713686d6854030786f16620
kazuma murata <mrt_014_kzm@yahoo.co.jp>
2016-12-03 20:47:38 +0900

    api変更

--- ch-django/drawing/views.py
+++ ch-django/drawing/views.py
@@ -6,8 +6,8 @@ import unirest
 import json
 
 # URLのv1より後はget処理内に記述する
-API_URL = "///v1" # 非公開
-ACCESS_TOKEN = "" # 非公開
+API_URL = ""
+ACCESS_TOKEN = ""
 API_HEADER = {"Authorization": ACCESS_TOKEN}
 
 
@@ -22,14 +22,21 @@ def userworks(request, user_id):
 
     # APIを呼び出してレスポンスオブジェクトを得る
     response = unirest.get(API_URL + "/users/" + user_id + "/works?" + query_string, headers=API_HEADER)
+    json_data = response.body
+
+    image_urls = []
+    works = json_data["response"]
+    for work in works:
+        image_urls.append(work["image_urls"]["small"])
+
+    image_json = json.dumps({'image_urls': image_urls})
 
     # HTTPコードをチェックして200以外だったらエラーということで例外を上げる
     if response.code != 200:
         raise Exception("HTTP Error %d : %s" % (response.code, response.raw_body))
 
-    json_data = json.dumps(response.body)
     result = {
-        "data": json_data
+        "data": image_json
     }
 
     return render(request, 'drawing/data_output.html', result)
@@ -37,18 +44,21 @@ def userworks(request, user_id):
 
 # ランキング作品抽出ビュー
 def ranking(request):
-    query_string = urllib.urlencode({"mode": "rookie"})
-
-    response = unirest.get(API_URL + "/ranking/all?" + query_string, headers=API_HEADER)
-
-    if response.code != 200:
-        raise Exception("HTTP Error %d : %s" % (response.code, response.raw_body))
-
-    json_data = response.body
     image_urls = []
-    works = json_data["response"][0]["works"]
-    for work in works:
-        image_urls.append(work["work"]["image_urls"]["small"])
+
+    # 1度のクエリで50件までしか取得できないので，2回回す
+    for i in range(2):
+        query_string = urllib.urlencode({"mode": "rookie", "page": (i+1)})
+
+        response = unirest.get(API_URL + "/ranking/all?" + query_string, headers=API_HEADER)
+
+        if response.code != 200:
+            raise Exception("HTTP Error %d : %s" % (response.code, response.raw_body))
+
+        json_data = response.body
+        works = json_data["response"][0]["works"]
+        for work in works:
+            image_urls.append(work["work"]["image_urls"]["small"])
 
     image_json = json.dumps({'image_urls': image_urls})
 
