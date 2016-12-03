c5123981a896030b988a6f40b7f06197f7babbcd
kazuma murata <mrt_014_kzm@yahoo.co.jp>
2016-12-04 05:52:32 +0900

    不要ファイル削除

--- ch-django/drawing/urls.py
+++ ch-django/drawing/urls.py
@@ -3,7 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
     url(r'^users/(?P<user_id>[0-9]+)/works/$', views.userworks, name='userworks'),
     url(r'^ranking/(?P<kind>[a-z].*)/high/$', views.ranking_high, name='ranking_high'),
     url(r'^ranking/(?P<kind>[a-z].*)/mid/$', views.ranking_mid, name='ranking_mid'),
